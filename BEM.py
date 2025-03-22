ae11cf2ebcf767356a9e7e905be8086c8cdc103a
Maxim-super-elite-programmer <M.Michielsen-1@student.tudelft.nl>
2025-03-22 17:55:47 +0100

    added constants and variables for the propeller

--- BEM.py
+++ BEM.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # propeller constants
-r_prop = 0.7
+r_prop = 0.7 #m
 N_blades_prop = 6
 blade_start_prop = 0.25 #r/R
 twist_prop = [-50, 35] #coefficient, degrees
@@ -16,3 +16,16 @@ rpm_prop = 1200
 altitude_prop = 2000 #m
 incidience_angle_prop = 0 #degrees
 yaw_angle_prop = [0, 15, 30] #degrees
+
+def BEM(r, N_blades, blade_start, twist, collective_blade_pitch, chord_distribution, airfoil, u, rpm, altitude, incidience_angle, yaw_angle):
+    N_dr = 100
+    dr = r/N_dr
+    r = np.linspace(blade_start_prop*r, r, N_dr)
+
+    return r
+
+
+
+BEM(r_prop, N_blades_prop, blade_start_prop, twist_prop, collective_blade_pitch_prop,
+     chord_distribution_prop, airfoil_prop, u_prop, rpm_prop, altitude_prop, incidience_angle_prop, yaw_angle_prop)
+
