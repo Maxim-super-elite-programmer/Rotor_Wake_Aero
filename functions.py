7c1f6ec229373cbb26e3393b2c7b3cdc86c7a53c
Maxim-super-elite-programmer <M.Michielsen-1@student.tudelft.nl>
2025-03-23 16:57:07 +0100

    mandatory part done

---
+++ functions.py
@@ -0,0 +1,103 @@
+import numpy as np
+
+def solve_stream(Uinf, r1_R, r2_R, rootradius_R, tipradius_R , Omega, Radius, NBlades, chord, twist, polar_alpha, polar_cl, polar_cd ):
+    """
+    solve balance of momentum between blade element load and loading in the streamtube
+    input variables:
+    Uinf - wind speed at infinity
+    r1_R,r2_R - edges of blade element, in fraction of Radius ;
+    rootradius_R, tipradius_R - location of blade root and tip, in fraction of Radius ;
+    Radius is the rotor radius
+    Omega -rotational velocity
+    NBlades - number of blades in rotor
+    """
+    Area = np.pi*((r2_R*Radius)**2-(r1_R*Radius)**2) #  area streamtube
+    r_R = (r1_R+r2_R)/2 # centroide
+    # initiatlize variables
+    a = 0.3 # axial induction
+    a_line = 0.0 # tangential induction factor
+    
+    Niterations = 100
+    Erroriterations =0.00001 # error limit for iteration rpocess, in absolute value of induction
+    
+    for i in range(Niterations):
+        Urotor = Uinf*(1-a) # axial velocity at rotor
+        Utan = (1+a_line)*Omega*r_R*Radius # tangential velocity at rotor
+
+        # calculate loads in blade segment in 2D (N/m)
+        fnorm, ftan, gamma = blade_loading(Urotor, Utan, r_R,chord, twist, polar_alpha, polar_cl, polar_cd)
+        load3Daxial =fnorm*Radius*(r2_R-r1_R)*NBlades # 3D force in axial direction
+
+        # // calculate thrust coefficient at the streamtube 
+        CT = load3Daxial/(0.5*Area*Uinf**2)
+        
+        # calculate new axial induction, accounting for Glauert's correction
+        a_new =  induction(CT)
+        
+        # correct new axial induction with Prandtl's correction
+        Prandtl, Prandtltip, Prandtlroot = prandtl_correction(r_R, rootradius_R, tipradius_R, Omega*Radius/Uinf, NBlades, a_new);
+        if (Prandtl < 0.0001): 
+            Prandtl = 0.0001 # avoid divide by zero
+        a_new = a_new/Prandtl # correct estimate of axial induction
+        a = 0.75*a+0.25*a_new # for improving convergence, weigh current and previous iteration of axial induction
+
+        # calculate aximuthal induction
+        a_line = ftan*NBlades/(2*np.pi*Uinf*(1-a)*Omega*2*(r_R*Radius)**2)
+        a_line =a_line/Prandtl # correct estimate of azimuthal induction with Prandtl's correction
+
+    return [a , a_line, r_R, fnorm , ftan, gamma]
+
+def blade_loading(vnorm, vtan, r_R, chord, twist, polar_alpha, polar_cl, polar_cd):
+    """
+    calculates the load in the blade element
+    """
+    vmag2 = vnorm**2 + vtan**2
+    inflowangle = np.arctan2(vnorm,vtan)
+    alpha = twist + inflowangle*180/np.pi
+    cl = np.interp(alpha, polar_alpha, polar_cl)
+    cd = np.interp(alpha, polar_alpha, polar_cd)
+    lift = 0.5*vmag2*cl*chord
+    drag = 0.5*vmag2*cd*chord
+    fnorm = lift*np.cos(inflowangle)+drag*np.sin(inflowangle)
+    ftan = lift*np.sin(inflowangle)-drag*np.cos(inflowangle)
+    gamma = 0.5*np.sqrt(vmag2)*cl*chord
+    return fnorm , ftan, gamma
+
+def prandtl_correction(r_R, rootradius_R, tipradius_R, TSR, NBlades, axial_induction):
+    """
+    This function calcualte steh combined tip and root Prandtl correction at agiven radial position 'r_R' (non-dimensioned by rotor radius), 
+    given a root and tip radius (also non-dimensioned), a tip speed ratio TSR, the number lf blades NBlades and the axial induction factor
+    """
+    temp1 = -NBlades/2*(tipradius_R-r_R)/r_R*np.sqrt( 1+ ((TSR*r_R)**2)/((1-axial_induction)**2))
+    Ftip = np.array(2/np.pi*np.arccos(np.exp(temp1)))
+    Ftip[np.isnan(Ftip)] = 0
+    temp1 = NBlades/2*(rootradius_R-r_R)/r_R*np.sqrt( 1+ ((TSR*r_R)**2)/((1-axial_induction)**2))
+    Froot = np.array(2/np.pi*np.arccos(np.exp(temp1)))
+    Froot[np.isnan(Froot)] = 0
+    return Froot*Ftip, Ftip, Froot
+
+def induction(CT):
+    """
+    This function calculates the induction factor 'a' as a function of thrust coefficient CT 
+    including Glauert's correction
+    """
+    a = np.zeros(np.shape(CT))
+    CT1=1.816
+    CT2=2*np.sqrt(CT1)-CT1
+    a[CT>=CT2] = 1 + (CT[CT>=CT2]-CT1)/(4*(np.sqrt(CT1)-1))
+    a[CT<CT2] = 0.5-0.5*np.sqrt(1-CT[CT<CT2])
+    return a
+
+def CT_function(a, glauert = False):
+    """
+    This function calculates the thrust coefficient as a function of induction factor 'a'
+    'glauert' defines if the Glauert correction for heavily loaded rotors should be used; default value is false
+    """
+    CT = np.zeros(np.shape(a))
+    CT = 4*a*(1-a)  
+    if glauert:
+        CT1=1.816
+        a1=1-np.sqrt(CT1)/2
+        CT[a>a1] = CT1-4*(np.sqrt(CT1)-1)*(1-a[a>a1])
+    
+    return CT
