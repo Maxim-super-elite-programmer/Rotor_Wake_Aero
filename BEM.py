6b3ea3e590afb63b85fd591ffc52cfd827a193e6
Maxim-super-elite-programmer <M.Michielsen-1@student.tudelft.nl>
2025-03-23 16:30:22 +0100

    lol gonna use existing code

--- BEM.py
+++ BEM.py
@@ -1,31 +1,184 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np  
+import pandas as pd
 
-# propeller constants
-r_prop = 0.7 #m
-N_blades_prop = 6
-blade_start_prop = 0.25 #r/R
-twist_prop = [-50, 35] #coefficient, degrees
-collective_blade_pitch_prop = 46 #degrees at r/R = 0.7
-chord_distribution_prop = [0.18, -0.06] #coefficient
-airfoil_prop = 'ARA-D8%'
+def CTfunction(a, glauert = False):
+    """
+    This function calculates the thrust coefficient as a function of induction factor 'a'
+    'glauert' defines if the Glauert correction for heavily loaded rotors should be used; default value is false
+    """
+    CT = np.zeros(np.shape(a))
+    CT = 4*a*(1-a)  
+    if glauert:
+        CT1=1.816
+        a1=1-np.sqrt(CT1)/2
+        CT[a>a1] = CT1-4*(np.sqrt(CT1)-1)*(1-a[a>a1])
+    
+    return CT
+  
+def ainduction(CT):
+    """
+    This function calculates the induction factor 'a' as a function of thrust coefficient CT 
+    including Glauert's correction
+    """
+    a = np.zeros(np.shape(CT))
+    CT1=1.816
+    CT2=2*np.sqrt(CT1)-CT1
+    a[CT>=CT2] = 1 + (CT[CT>=CT2]-CT1)/(4*(np.sqrt(CT1)-1))
+    a[CT<CT2] = 0.5-0.5*np.sqrt(1-CT[CT<CT2])
+    return a
 
-# propeller operational constants
-u_prop = 60 #m/s
-rpm_prop = 1200
-altitude_prop = 2000 #m
-incidience_angle_prop = 0 #degrees
-yaw_angle_prop = [0, 15, 30] #degrees
+def PrandtlTipRootCorrection(r_R, rootradius_R, tipradius_R, TSR, NBlades, axial_induction):
+    """
+    This function calcualte steh combined tip and root Prandtl correction at agiven radial position 'r_R' (non-dimensioned by rotor radius), 
+    given a root and tip radius (also non-dimensioned), a tip speed ratio TSR, the number lf blades NBlades and the axial induction factor
+    """
+    temp1 = -NBlades/2*(tipradius_R-r_R)/r_R*np.sqrt( 1+ ((TSR*r_R)**2)/((1-axial_induction)**2))
+    Ftip = np.array(2/np.pi*np.arccos(np.exp(temp1)))
+    Ftip[np.isnan(Ftip)] = 0
+    temp1 = NBlades/2*(rootradius_R-r_R)/r_R*np.sqrt( 1+ ((TSR*r_R)**2)/((1-axial_induction)**2))
+    Froot = np.array(2/np.pi*np.arccos(np.exp(temp1)))
+    Froot[np.isnan(Froot)] = 0
+    return Froot*Ftip, Ftip, Froot
 
-def BEM(r, N_blades, blade_start, twist, collective_blade_pitch, chord_distribution, airfoil, u, rpm, altitude, incidience_angle, yaw_angle):
-    N_dr = 100
-    dr = r/N_dr
-    r = np.linspace(blade_start_prop*r, r, N_dr)
+airfoil = 'ARAD8pct_polar.txt'
+data1=pd.read_csv(airfoil, skiprows=2,
+                    names = ["alfa", "cl", "cd", "cm"],  sep=r'\s+')
+polar_alpha = data1['alfa'][:]
+polar_cl = data1['cl'][:]
+polar_cd = data1['cd'][:]
 
-    return r
+# define function to determine load in the blade element
+def loadBladeElement(vnorm, vtan, r_R, chord, twist, polar_alpha, polar_cl, polar_cd):
+    """
+    calculates the load in the blade element
+    """
+    vmag2 = vnorm**2 + vtan**2
+    inflowangle = np.arctan2(vnorm,vtan)
+    alpha = twist + inflowangle*180/np.pi
+    cl = np.interp(alpha, polar_alpha, polar_cl)
+    cd = np.interp(alpha, polar_alpha, polar_cd)
+    lift = 0.5*vmag2*cl*chord
+    drag = 0.5*vmag2*cd*chord
+    fnorm = lift*np.cos(inflowangle)+drag*np.sin(inflowangle)
+    ftan = lift*np.sin(inflowangle)-drag*np.cos(inflowangle)
+    gamma = 0.5*np.sqrt(vmag2)*cl*chord
+    return fnorm , ftan, gamma
 
+def solveStreamtube(Uinf, r1_R, r2_R, rootradius_R, tipradius_R , Omega, Radius, NBlades, chord, twist, polar_alpha, polar_cl, polar_cd ):
+    """
+    solve balance of momentum between blade element load and loading in the streamtube
+    input variables:
+    Uinf - wind speed at infinity
+    r1_R,r2_R - edges of blade element, in fraction of Radius ;
+    rootradius_R, tipradius_R - location of blade root and tip, in fraction of Radius ;
+    Radius is the rotor radius
+    Omega -rotational velocity
+    NBlades - number of blades in rotor
+    """
+    Area = np.pi*((r2_R*Radius)**2-(r1_R*Radius)**2) #  area streamtube
+    r_R = (r1_R+r2_R)/2 # centroide
+    # initiatlize variables
+    a = 0.3 # axial induction
+    aline = 0.0 # tangential induction factor
+    
+    Niterations = 100
+    Erroriterations =0.00001 # error limit for iteration rpocess, in absolute value of induction
+    
+    for i in range(Niterations):
+        Urotor = Uinf*(1-a) # axial velocity at rotor
+        Utan = (1+aline)*Omega*r_R*Radius # tangential velocity at rotor
 
+        # calculate loads in blade segment in 2D (N/m)
+        fnorm, ftan, gamma = loadBladeElement(Urotor, Utan, r_R,chord, twist, polar_alpha, polar_cl, polar_cd)
+        load3Daxial =fnorm*Radius*(r2_R-r1_R)*NBlades # 3D force in axial direction
 
-BEM(r_prop, N_blades_prop, blade_start_prop, twist_prop, collective_blade_pitch_prop,
-     chord_distribution_prop, airfoil_prop, u_prop, rpm_prop, altitude_prop, incidience_angle_prop, yaw_angle_prop)
+        # // calculate thrust coefficient at the streamtube 
+        CT = load3Daxial/(0.5*Area*Uinf**2)
+        
+        # calculate new axial induction, accounting for Glauert's correction
+        anew =  ainduction(CT)
+        
+        # correct new axial induction with Prandtl's correction
+        Prandtl, Prandtltip, Prandtlroot = PrandtlTipRootCorrection(r_R, rootradius_R, tipradius_R, Omega*Radius/Uinf, NBlades, anew);
+        if (Prandtl < 0.0001): 
+            Prandtl = 0.0001 # avoid divide by zero
+        anew = anew/Prandtl # correct estimate of axial induction
+        a = 0.75*a+0.25*anew # for improving convergence, weigh current and previous iteration of axial induction
 
+        # calculate aximuthal induction
+        aline = ftan*NBlades/(2*np.pi*Uinf*(1-a)*Omega*2*(r_R*Radius)**2)
+        aline =aline/Prandtl # correct estimate of azimuthal induction with Prandtl's correction
+
+        #// test convergence of solution, by checking convergence of axial induction
+        if (np.abs(a-anew) < Erroriterations): 
+            #print("iterations")
+            #print(i)
+            break
+
+    return [a , aline, r_R, fnorm , ftan, gamma]
+
+# define the blade geometry
+delta_r_R = .01
+r_R = np.arange(0.2, 1+delta_r_R/2, delta_r_R)
+
+# blade shape
+pitch = 2 # degrees
+chord_distribution = 3*(1-r_R)+1 # meters
+twist_distribution = -14*(1-r_R)+pitch # degrees
+
+# define flow conditions
+Uinf = 1 # unperturbed wind speed in m/s
+TSR = 8 # tip speed ratio
+Radius = 50
+Omega = Uinf*TSR/Radius
+NBlades = 3
+
+TipLocation_R =  1
+RootLocation_R =  0.2
+
+# solve BEM model
+results =np.zeros([len(r_R)-1,6]) 
+
+for i in range(len(r_R)-1):
+    chord = np.interp((r_R[i]+r_R[i+1])/2, r_R, chord_distribution)
+    twist = np.interp((r_R[i]+r_R[i+1])/2, r_R, twist_distribution)
+    
+    results[i,:] = solveStreamtube(Uinf, r_R[i], r_R[i+1], RootLocation_R, TipLocation_R , Omega, Radius, NBlades, chord, twist, polar_alpha, polar_cl, polar_cd )
+
+# plot results
+areas = (r_R[1:]**2-r_R[:-1]**2)*np.pi*Radius**2
+dr = (r_R[1:]-r_R[:-1])*Radius
+CT = np.sum(dr*results[:,3]*NBlades/(0.5*Uinf**2*np.pi*Radius**2))
+CP = np.sum(dr*results[:,4]*results[:,2]*NBlades*Radius*Omega/(0.5*Uinf**3*np.pi*Radius**2))
+
+print("CT is ", CT)
+print("CP is ", CP)
+
+plot = 0
+if plot:
+    fig1 = plt.figure(figsize=(12, 6))
+    plt.title('Axial and tangential induction')
+    plt.plot(results[:,2], results[:,0], 'r-', label=r'$a$')
+    plt.plot(results[:,2], results[:,1], 'g--', label=r'$a^,$')
+    plt.grid()
+    plt.xlabel('r/R')
+    plt.legend()
+    plt.show()
+
+    fig1 = plt.figure(figsize=(12, 6))
+    plt.title(r'Normal and tagential force, non-dimensioned by $\frac{1}{2} \rho U_\infty^2 R$')
+    plt.plot(results[:,2], results[:,3]/(0.5*Uinf**2*Radius), 'r-', label=r'Fnorm')
+    plt.plot(results[:,2], results[:,4]/(0.5*Uinf**2*Radius), 'g--', label=r'Ftan')
+    plt.grid()
+    plt.xlabel('r/R')
+    plt.legend()
+    plt.show()
+
+    fig1 = plt.figure(figsize=(12, 6))
+    plt.title(r'Circulation distribution, non-dimensioned by $\frac{\pi U_\infty^2}{\Omega * NBlades } $')
+    plt.plot(results[:,2], results[:,5]/(np.pi*Uinf**2/(NBlades*Omega)), 'r-', label=r'$\Gamma$')
+    plt.grid()
+    plt.xlabel('r/R')
+    plt.legend()
+    plt.show()
